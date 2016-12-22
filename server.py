fe42fce8f98db4e2514801c94e00286eb401e8c6
prashant naik <prasnaik@in.ibm.com>
2016-12-22 04:14:43 -0600

    commit

--- server.py
+++ server.py
@@ -24,7 +24,29 @@ client = MongoClient(mongodb_url)
 #get the default database:
 #db = client.get_default_database()  
 #db = client.test
-db = client.EmployeeData
+db = client.Employees
+
+# Function to insert data into mongo db
+def insert():
+    try:
+    employeeId = raw_input('839441 :')
+    employeeName = raw_input('prashant :')
+    employeeAge = raw_input('34 :')
+    employeeCountry = raw_input('India :')
+        
+    db.Employees.insert_one(
+        {
+        "id": employeeId,
+            "name":employeeName,
+        "age":employeeAge,
+        "country":employeeCountry
+        })
+        print '\nInserted data successfully\n'
+    
+    except Exception, e:
+        print str(e)
+
+
 
 print('connected to mongodb!, welcome to mongodb connection, have a fun and bit relax')
 #db.test_collection.insert({}) 
